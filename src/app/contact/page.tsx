97afc7d5a0e6825a4f7eecc579dbc8a99db2403e
Alex <alexsexwale@gmail.com>
2025-12-26 17:33:31 +0200

    Update contact page to include additional cities in location and streamline email information by removing redundant entries. Commented out the 'Call Us' section for future reference.

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -80,7 +80,7 @@ export default function ContactPage() {
                 <div>
                   <h3 className="font-heading text-lg mb-2">Our Location</h3>
                   <p className="text-gray-400">
-                    Cape Town<br />
+                    Cape Town, Johannesburg, Durban<br />
                     South Africa
                   </p>
                 </div>
@@ -97,9 +97,7 @@ export default function ContactPage() {
                 <div>
                   <h3 className="font-heading text-lg mb-2">Email Us</h3>
                   <p className="text-gray-400">
-                    General: support@darkpoint.co.za<br />
-                    Support: support@darkpoint.co.za<br />
-                    Sales: support@darkpoint.co.za
+                    Support: support@darkpoint.co.za
                   </p>
                 </div>
               </div>
@@ -112,13 +110,13 @@ export default function ContactPage() {
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                   </svg>
                 </div>
-                <div>
+                {/* <div>
                   <h3 className="font-heading text-lg mb-2">Call Us</h3>
                   <p className="text-gray-400">
                     WhatsApp: +27 XX XXX XXXX<br />
                     Hours: Mon-Fri 9AM-5PM SAST
                   </p>
-                </div>
+                </div> */}
               </div>
             </div>
 
